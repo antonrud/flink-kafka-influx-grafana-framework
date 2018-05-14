9dfa6b383a5e06e483ed4a4d5e0ceb1b19603cde
Anton Rudacov <a.rudacov@campus.tu-berlin.de>
2018-05-14 10:22:10 +0200

    Maven project started

---
+++ src/main/java/de/tuberlin/tubit/gitlab/anton/rudacov/App.java
@@ -0,0 +1,8 @@
+package de.tuberlin.tubit.gitlab.anton.rudacov;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/de/tuberlin/tubit/gitlab/anton/rudacov/AppTest.java
@@ -0,0 +1,18 @@
+package de.tuberlin.tubit.gitlab.anton.rudacov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    @Test
+    public void shouldAnswerWithTrue() {
+
+        assertTrue(true);
+    }
+}
