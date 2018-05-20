46a0bdfb70a8e8b4274ea6542630b82c0986dd08
Anton Rudacov <a.rudacov@campus.tu-berlin.de>
2018-05-21 01:04:51 +0200

    Started DataGenerator, not working by now.

--- src/main/java/de/tuberlin/tubit/gitlab/anton/rudacov/DataGenerator.java
+++ src/main/java/de/tuberlin/tubit/gitlab/anton/rudacov/DataGenerator.java
@@ -1,5 +1,12 @@
 package de.tuberlin.tubit.gitlab.anton.rudacov;
 
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.connectors.kafka.KafkaSink;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,6 +14,14 @@ import java.util.stream.Stream;
 
 public class DataGenerator implements Runnable {
 
+    /* Standart args set:
+    --topic
+    test
+    --bootstrap.servers
+    localhost:9092
+     */
+
+    String[] args;
     private String dataPath;
 
     public DataGenerator(String dataPath) {
@@ -14,18 +29,19 @@ public class DataGenerator implements Runnable {
         this.dataPath = dataPath;
     }
 
-    @Override
-    public void run() {
-        App.log('i', "Generator starting...");
-
+    private void produce() throws Exception {
         Stream<String> dataStream = getDataStream(dataPath);
 
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        DataStream<String> messageStream = env.addSource(new SimpleStringGenerator());
 
-        while (!Thread.currentThread().isInterrupted()) {
+        // write stream to Kafka
+        messageStream.addSink(new KafkaSink(parameterTool.getRequired("bootstrap.servers"),
+                parameterTool.getRequired("topic"),
+                new SimpleStringSchema()));
 
-
-            Thread.currentThread().interrupt();
-        }
+        env.execute();
     }
 
     private Stream getDataStream(String file) {
@@ -39,4 +55,35 @@ public class DataGenerator implements Runnable {
 
         return dataStream;
     }
+
+    @Override
+    public void run() {
+        App.log('i', "Generator starting...");
+
+        try {
+            produce();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class SimpleStringGenerator implements SourceFunction<String> {
+        private static final long serialVersionUID = 2174904787118597072L;
+        boolean running = true;
+        long i = 0;
+
+        @Override
+        public void run(SourceFunction.SourceContext<String> ctx) throws Exception {
+            while (running) {
+                ctx.collect("element-" + (i++));
+                Thread.sleep(10);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            running = false;
+        }
+    }
+
 }
